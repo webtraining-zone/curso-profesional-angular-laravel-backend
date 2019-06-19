702a1510a2f5564535ea51b8b1c4a945faeb8090
Alex Arriaga <alexzander.arriaga@base22.com>
2019-06-19 18:51:09 -0500

    Crear migración para proyectos multi idioma

---
+++ database/migrations/2019_06_19_230446_create_projects_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('slug');
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+
+        Schema::create('project_translations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('project_id')->unsigned();
+            $table->string('locale')->index();
+
+            $table->string('title');
+            $table->text('description');
+
+            $table->unique(['project_id', 'locale']);
+
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
